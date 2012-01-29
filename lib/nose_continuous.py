51f9188cdd53f623e3c9ec6080fc40aadac60b3d
ReSTARTR <yoshida.masaki+restartr@gmail.com>
2012-01-29 21:15:28 +0900

    solved the probrem for vim swapping(modified event become deleted and created event)

--- lib/nose_continuous.py
+++ lib/nose_continuous.py
@@ -1,32 +1,30 @@
-import subprocess
 import time
+import logging
+import subprocess
 from watchdog.events        import PatternMatchingEventHandler
 from watchdog.observers     import Observer
 
-argv = []
+class UniqueTimeHandler(PatternMatchingEventHandler):
 
-def run_test():
-    global argv
-    cmd = ['./env/bin/nosetests', '-s', '--rednose']
-    map(cmd.append, argv)
-    subprocess.call(cmd)
-    print '-' * 10
-
-class MyHandler(PatternMatchingEventHandler):
-
-    def __init__(self, patterns=None, ignore_patterns=None,
+    def __init__(self, event_queue, argv, patterns=None, ignore_patterns=None,
                          ignore_directories=False, case_sensitive=False):
-        self.is_running = False
-        super(MyHandler,self).__init__(patterns, ignore_patterns, ignore_directories, case_sensitive)
-        
+        """
+        args:
+            event_queue : brownie.datastructures.SetQueue
+            argv        : list
+        """
+        self.event_queue = event_queue
+        self.argv = argv
+        super(UniqueTimeHandler,self).__init__(patterns, ignore_patterns, ignore_directories, case_sensitive)
+
     def on_any_event(self, event):
-        if self.is_running == True:
-            return
+        self.event_queue.queue.clear()
         self.is_running = True
-        print "[%s] %s" % (event._event_type.upper(), event._src_path)
-        run_test()
-        time.sleep(1)
-        self.is_running = False
+        logging.info("[%s] %s" % (event._event_type.upper(), event._src_path))
+        cmd = ['./env/bin/nosetests', '-s', '--rednose']
+        map(cmd.append, self.argv)
+        subprocess.call(cmd)
+        print '-' * 10
 
 class NoseContinuous(object):
 
@@ -34,21 +32,20 @@ class NoseContinuous(object):
         self.target_patterns = patterns
         self.target_dirs = dirs
         self.argv = []
-        if len(options)>0:
+        if len(options) > 0:
             self.argv = options
 
     def run(self):
-        event_handler = MyHandler(patterns=self.target_patterns)
-        observer = Observer()
+        observer = Observer(timeout=3)
+        event_handler = UniqueTimeHandler(observer.event_queue, self.argv,
+                                            patterns=self.target_patterns)
         for dir in self.target_dirs:
             observer.schedule(event_handler, path=dir, recursive=True)
         observer.schedule(event_handler, path='.', recursive=False)
-
         observer.start()
         try:
             while True:
-                time.sleep(0.5)
+                time.sleep(1)
         except KeyboardInterrupt:
             observer.stop()
         observer.join()
-
